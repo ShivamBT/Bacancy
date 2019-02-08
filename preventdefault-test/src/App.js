e838cfb74fbd4a9d141de3a48ede9ee745fa86cc
ShivamBT <shivam.pande@bacancytechnology.com>
2019-02-08 16:03:26 +0530

    Validation methods added

--- preventdefault-test/src/App.js
+++ preventdefault-test/src/App.js
@@ -4,11 +4,15 @@ import { Textarea } from "./Components/Textarea";
 
 import "./App.css";
 import { Button } from "reactstrap";
-import { Form, FormGroup, Label } from "reactstrap";
+import { Form, FormGroup, Label , Container ,Row,Col} from "reactstrap";
 
 const pstyle={
   color: 'blue',
   textAlign: 'center'
+}
+
+const dstyle={
+  color: 'red',
 }
 
 
@@ -19,7 +23,7 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data:{
+      data:{                                               //Data Object is used to store data temporarily
       name: "",
       address: "",
       email: "",
@@ -28,25 +32,42 @@ export class App extends Component {
       phone_number: "",
       gender: "",
       like: [],
-      valid_name: false,
-      valid_email:false,
-      valid_password:false,
-      valid_number:false
-
+      
     },
-      signup:[]
+      signup:[],                                          //Signup array Stores User Data in each of its element
+
+
+      valid_name:true,                                  // These valid_variables are used to store boolean values which are result of validation functions
+      valid_email:true,
+      valid_password:true,
+      valid_confirm_password:true,
+      valid_phone_number:true,
+      valid_password_match:true,
+      field:true,
 
     }
 
     this.changeValue=this.changeValue.bind(this);
     this.changeValue2=this.changeValue2.bind(this);
     this.submitValue=this.submitValue.bind(this);
+    this.validate=this.validate.bind(this);
     this.validateName=this.validateName.bind(this);
     this.validateEmail=this.validateEmail.bind(this);
     this.validatePassword=this.validatePassword.bind(this);
-    this.validateNUmber=this.validateNUmber.bind(this);
+    this.validateConfirmPassword=this.validateConfirmPassword.bind(this);
+    this.validateNumber=this.validateNumber.bind(this);
+    this.submitValidation=this.submitValidation.bind(this);
+    this.userCheck=this.userCheck.bind(this);
+    
   }
 
+
+
+
+
+
+
+//Change Value Functions Start here
 
   changeValue(e)
   {
@@ -75,65 +96,335 @@ export class App extends Component {
     this.setState({data});
   }
 
-
+//Change Value Functions end here
   
 
+
+//Submit Function Start
   async submitValue(e)
   {
-
-   
-
- // let signup=[...this.state.signup,{...this.state.data}];
+    let data={...this.state.data};
   
-     await this.setState({signup : [...this.state.signup,this.state.data]});
+    if(this.submitValidation() && this.userCheck(data))
+    {
+      await this.setState({signup : [...this.state.signup,this.state.data]});
 
-    console.log("Value Submitted: " , this.state)
+      console.log("Value Submitted: " , this.state)
+      
+      alert("User registed Successfully !!");
+    }
+    
      
   }
 
-  
+ //Submit Function end
 
-  /*validateName()
+  // Main Validation Function starts here
+
+  async validate(e)
   {
+      if(e.target.name==="name")
+      {
+          let x=e.target.value;
 
-  }*/
+          this.validateName(x);
+      }
 
- 
+      else if(e.target.name==="email")
+      {
+          let x=e.target.value;
 
-  render() {
+          this.validateEmail(x);
+      }
+
+     else if(e.target.name==="password")
+      {
+          let x=e.target.value;
+
+          this.validatePassword(x);
+      }
+
+      else if(e.target.name==="confirm_password")
+      {
+          let x=e.target.value;
+          
+          this.validateConfirmPassword(x);
+
+           
+     }
+
+      else if(e.target.name==="phone_number")
+      {
+          let x=e.target.value;
+
+          this.validateNumber(x);
+      }
+  }
+
+//Main Validation function ends here
+
+
+
+//Individual Validation methods start here
+
+  async validateName(x)
+    {   
+       await this.setState({valid_name:false});
+
+       let regex= /^[a-zA-Z ]{2,30}$/.test(x);
+             
+       await this.setState({valid_name:regex});
+
+       console.log("regex is: ",regex);
+    }
+
+    async validateEmail(x)
+    {
+        await this.setState({valid_email:false});
+        
+        let regex= /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(x);
+
+        await this.setState({valid_email:regex});
+
+        console.log("regex is: ",regex);
+    }
+
+   async validatePassword(x)
+    {
+        await this.setState({valid_password:false});
+       
+        console.log("Value of password: ",x);
+
+        let regex= /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(x);
+
+        await this.setState({valid_password:regex});
+
+        console.log("regex is: ",regex);
+    }
+
+
+    async validateConfirmPassword(x)
+    {
+        await this.setState({valid_confirm_password:false,valid_password_match:false});
+
+        let regex= /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(x);
+
+        console.log("Value of password: ", this.state.data.password);
+        console.log("Value of confirm password: ",x);
+
+        if(regex && this.state.data.password===this.state.data.confirm_password)
+        {
+                        
+          
+          await this.setState({valid_confirm_password:regex,valid_password_match:true});
+        }
+
+        else if(regex)
+        {
+
+         // alert("Passwords Do Not Match");
+               
+          await this.setState({valid_confirm_password:regex});
+
+        }
+
+    }
+    
+
+    async validateNumber(x)
+    {
+        await this.setState({valid_phone_number:false});
+
+        let regex= /^(?:(?:\+|0{0,2})91(\s*[\ -]\s*)?|[0]?)?[789]\d{9}|(\d[ -]?){10}\d$/.test(x);
+
+        await this.setState({valid_phone_number:regex});
+
+        console.log("regex is: ",regex);
+    }
+
+  
+    async submitValidation()
+    {
+      console.log("Submit Validation called");
+
+      await this.setState({field:false});
+
+      if(this.state.data.name=='')
+      {
+        console.log("If called");
+          await this.setState({field:false});
+          return false;
+      } 
+      
+      
+      else if(this.state.data.email=='')
+      {
+        console.log("If called");
+          await this.setState({field:false});
+          return false;
+      }
+
+      else if(this.state.data.password=='')
+      {
+        console.log("If called");
+          await this.setState({field:false});
+          return false;
+      } 
+      
+       
+      else if(this.state.data.confirm_password=='') 
+      {
+        console.log("If called");
+          await this.setState({field:false});
+          return false;
+      }
+      
+      else if(this.state.data.phone_number=='')
+      {
+        console.log("If called");
+          await this.setState({field:false});
+          return false;
+      } 
+      
+      else if(this.state.data.gender=='')
+      {
+        console.log("If called");
+          await this.setState({field:false});
+          return false;
+      } 
+      
+
+      else
+      {
+        console.log("else called");
+        await this.setState({field:true});
+
+        return true;
+      }
+    }
+
+
+    userCheck(data)
+    {
+      for(let i=0 ; i<this.state.signup.length ; i++)
+      {
+        if(data.email==this.state.signup[i].email)
+        {
+          alert("This user is already registered");
+          console.log("User already registered");
+          return false;
+        }
+       }
+       
+       return true;
+    }
+
+//Individual Validation Methods end here
+
+   
+
+
+//Render Function starts from here
+
+   render() {
     return (
       <div className="App">
         <h1 style={pstyle}>Registration Form</h1>
+        <h2>{this.state.field ? null : <p style={dstyle}>All Fields except Likes and Address are compulsory</p>}</h2>
         
-       
-          <FormGroup>
+        
+        <Container>
+          <Form>        
+          <Row>
+            <Col>
+            <FormGroup>
               <Label>
-              <InputComponent label="Name "   type="text" name="name" onChange={e => this.changeValue(e)}/>
+              <InputComponent label="Name"  id="1"  type="text" name="name" onChange={e => this.changeValue(e)} onBlur={e => this.validate(e)}/>
               </Label>
+              {this.state.valid_name ? null : <p style={dstyle}><br/> Invalid Name</p>}
               
+              
+          
+            </FormGroup>
+
+            </Col>
+            <Col>
+            <FormGroup>
+              <Label>
+              <InputComponent   label="E-Mail" type="email" name="email" onChange={e => this.changeValue(e)} onBlur={e => this.validate(e)}/>
+              </Label>
+              {this.state.valid_email ? null : <p style={dstyle}><br/> Invalid Email</p>}
+           
+            </FormGroup>
+            </Col>
+          </Row>
+         
+          
+      
+          
+
+          
+          
+          
+
+         <Row>
+           <Col>
+
+           <FormGroup>
+              <Label>
+
+              <InputComponent
+                
+                label="Password"
+                type="password"
+                name="password"
+                value={this.state.password}
+
+                onChange={e => this.changeValue(e)}
+                onBlur={e => this.validate(e)}
+              />
+              </Label>
+              {this.state.valid_password ? null : <p style={dstyle}><br/> Invalid Password</p>}
           
           </FormGroup>
 
-          
+           </Col>
 
-
-          <FormGroup>
+           <Col>
+           <FormGroup>
               <Label>
-              <Textarea label="Address"  type="textarea" name="address" onChange={e => this.changeValue(e)}/>
+
+              <InputComponent
+
+                label="Confirm Password"
+                type="password"
+                name="confirm_password"
+                value={this.state.confirm_password}
+               
+
+                onChange={e => this.changeValue(e)}
+                onBlur={e => this.validate(e)}
+              
+                
+              />
               </Label>
-              
-          
-          </FormGroup>
-
-
-       
-          <FormGroup>
-            <Label>
-            <InputComponent   label="E-Mail" type="email" name="email" onChange={e => this.changeValue(e)} onBlur/>
-            </Label>
-              
+              {this.state.valid_confirm_password ? null : <p style={dstyle}><br/> Invalid Confirm Password</p>}
+              { this.state.valid_password_match ? null : <p style={dstyle}><br/> Passwords do not match</p>} 
            
           </FormGroup>
+
+           </Col>
+         </Row>
+
+
+         
+
+          
+        
+
+
+        <Row>
+          <Col>
 
           <FormGroup>
                 <Label>
@@ -143,49 +434,39 @@ export class App extends Component {
                 type="number"
                 name="phone_number"
 
-                onChange={e => this.changeValue(e)}/>
+                onChange={e => this.changeValue(e)}
+                onBlur={e => this.validate(e)}
+                />
                 </Label>
-              
-          
-          </FormGroup>
+              {this.state.valid_phone_number ? null : <p style={dstyle}><br/> Invalid Phone Number</p>}
 
+          </FormGroup>  
 
-          <FormGroup>
-              <Label>
+          </Col>
 
-              <InputComponent
-                
-                label="Password"
-                type="password"
-                name="password"
-
-                onChange={e => this.changeValue(e)}
-              />
-              </Label>
-              
-          
-          </FormGroup>
-
-          <FormGroup>
-              <Label>
-
-              <InputComponent
-
-                label="Confirm Password"
-                type="password"
-                name="confirm_password"
-
-                onChange={e => this.changeValue(e)}
-              />
-              </Label>
-              
+          <Col>
            
+          <FormGroup>
+              <Label>
+              <Textarea label="Address"  type="textarea" name="address" onChange={e => this.changeValue(e)} onBlur={e => this.validate(e)}/>
+              </Label>
+            
+              
+          
           </FormGroup>
 
+          </Col>
+        </Row>
+                    
           
+          
+          
+        
+ 
 
           <FormGroup tag="fieldset">
             <legend>Gender</legend>
+            {/* {this.state.radio ? null : <p style={dstyle}> Please select one of the Gender</p>} */}
             <FormGroup check>
               <Label>
                 <InputComponent
@@ -267,7 +548,10 @@ export class App extends Component {
           </FormGroup>
 
           <Button color="primary" name="signup"  onClick={e => this.submitValue(e)}>SignUp</Button>
-        
+        </Form>
+
+        </Container>
+             
       </div>
     );
   }
--- preventdefault-test/src/Components/InputComponent.js
+++ preventdefault-test/src/Components/InputComponent.js
@@ -1,27 +1,54 @@
-import React  from 'react';
-import { InputGroup, InputGroupAddon, Input} from 'reactstrap';
+import React,{Component}  from 'react';
+import { InputGroup, InputGroupAddon,InputGroupText,Input} from 'reactstrap';
+import './InputComponent.css';
 
-
-export const InputComponent =(props) =>
-{
-    return (
-        <div>
-          <InputGroup>
-
-                 <InputGroupAddon addonType="prepend">
-                     {props.label}
-                  </InputGroupAddon> 
-   
-       
-     
-                 <Input type={props.type}  name={props.name} value={props.value} onChange={props.onChange} />
-
-            </InputGroup>
-          
-           </div>
-        
-    );
+const pstyle={
+    fontSize: '15px',
+    color: 'red',
 }
-   
 
+export class InputComponent extends Component
+{
 
+  
+    
+    render()
+    {     
+        
+        return ( 
+        <div>
+
+            <InputGroup>
+              
+                   <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          {this.props.label}
+                        </InputGroupText>
+                     
+                    </InputGroupAddon> 
+                    
+                   
+                   
+                   
+                   
+                    <input type={this.props.type}   name={this.props.name} value={this.props.value}  onChange={this.props.onChange} onBlur={this.props.onBlur}/>
+                    
+                  
+                  
+                   
+             
+              
+               
+                   
+               </InputGroup>
+         
+
+            
+        </div>
+          
+      );
+  
+    }
+     
+    
+}

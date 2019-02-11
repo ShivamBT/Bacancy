4b2b5889bffab6295666a54bb09330929e08a53e
ShivamBT <shivam.pande@bacancytechnology.com>
2019-02-11 13:54:18 +0530

    form optimized

--- prerak_crud/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
-import Router from './route';
-
-ReactDOM.render(<Router />, document.getElementById('root'));
-serviceWorker.unregister();

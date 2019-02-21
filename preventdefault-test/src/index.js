ba838a7fcbbbbcb643ce16e62d10e2d416b7943e
ShivamBT <shivam.pande@bacancytechnology.com>
2019-02-21 18:53:56 +0530

    Webpack added

--- preventdefault-test/src/index.js
+++ preventdefault-test/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from "./App"
+import { App } from "./App";
 import { Routing } from "./Routing";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';

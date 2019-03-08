f1735733f07d01147e61ecdfc604802bbf06b4e6
ShivamBT <shivam.pande@bacancytechnology.com>
2019-03-08 19:08:06 +0530

    Server Side Pagination added

--- residence-one/src/components/Administration/Users.js
+++ residence-one/src/components/Administration/Users.js
@@ -4,7 +4,7 @@ import ReactTable from "react-table";
 import "react-table/react-table.css";
 import { Dashboard } from "../Dashboard/Dashboard";
 import "./Users.css";
-import { Button, ButtonGroup } from "reactstrap";
+import { Button, ButtonGroup, Nav, NavItem, NavLink } from "reactstrap";
 import { getUserList } from "../ApiCalls/ApiCalls";
 import { Sidebar } from "../Sidebar/Sidebar";
 import { LogOutComponent } from "../LogOutComponent/LogOutComponent";
@@ -19,8 +19,8 @@ export class Users extends Component {
       inactive_bool: false,
       imagePath: "",
       user_bool: true,
-      total_pages: '',
-      current_page:1
+      total_pages: "",
+      current_page: 1
     };
 
     this.clickHandler = this.clickHandler.bind(this);
@@ -29,42 +29,52 @@ export class Users extends Component {
   }
 
   async clickHandler(e) {
-    await this.setState({ user_bool: e.target.value ,current_page:1});
-    let result = await getUserList(this.state.user_bool, this.state.current_page,this.state.token);
+    await this.setState({ user_bool: e.target.value, current_page: 1 });
+    let result = await getUserList(
+      this.state.user_bool,
+      this.state.current_page,
+      this.state.token
+    );
     this.setState({
       data: result.data.data,
       active_bool: !this.state.active_bool,
       inactive_bool: !this.state.inactive_bool,
       imagePath: result.data.imagePath,
-      total_pages: Math.ceil((result.data.totalRecords) / 20)
+      total_pages: Math.ceil(result.data.totalRecords / 20)
     });
   }
 
   async componentDidMount() {
     await this.setState({ token: localStorage.getItem("token") });
-    let result = await getUserList(true,this.state.current_page, this.state.token);
+    let result = await getUserList(
+      true,
+      this.state.current_page,
+      this.state.token
+    );
     this.setState({
       data: result.data.data,
       imagePath: result.data.imagePath,
-      total_pages: Math.ceil((result.data.totalRecords) / 20)
+      total_pages: Math.ceil(result.data.totalRecords / 20)
     });
 
     console.log("Result is : ", result);
   }
 
-  async changeCurrentPage(pageIndex)
-  {
-    await this.setState({ current_page: pageIndex + 1 }); 
+  async changeCurrentPage(pageIndex) {
+    await this.setState({ current_page: pageIndex + 1 });
     this.fetchData();
   }
 
-  async fetchData()
-  {
-    let result = await getUserList(this.state.user_bool, this.state.current_page, this.state.token);
+  async fetchData() {
+    let result = await getUserList(
+      this.state.user_bool,
+      this.state.current_page,
+      this.state.token
+    );
     this.setState({
       data: result.data.data,
       imagePath: result.data.imagePath,
-      total_pages: Math.ceil((result.data.totalRecords) / 20)
+      total_pages: Math.ceil(result.data.totalRecords / 20)
     });
 
     console.log("Result is :", result);
@@ -79,7 +89,7 @@ export class Users extends Component {
         maxWidth: 250,
         minWidth: 250
       },
-     
+
       {
         Header: "Profile Picture",
         accessor: "picture",
@@ -100,8 +110,7 @@ export class Users extends Component {
 
       {
         Header: "Status",
-        accessor: "status",
-        
+        accessor: "status"
       },
 
       {
@@ -117,17 +126,16 @@ export class Users extends Component {
       },
 
       {
-        Header:"Building"
+        Header: "Building"
       },
 
       {
-        Header:"Type of Unit"
+        Header: "Type of Unit"
       },
 
       {
         Header: "Entry"
       },
-
 
       {
         Header: "Email",
@@ -139,14 +147,13 @@ export class Users extends Component {
 
       {
         Header: "Date of Birth",
-        accessor:"dateOfBirth"
+        accessor: "dateOfBirth"
       },
 
       {
         Header: "Mobile No",
-        accessor:"telephone"
+        accessor: "telephone"
       }
-      
     ];
     return (
       <div className="main">
@@ -155,40 +162,64 @@ export class Users extends Component {
         </div>
 
         <div className="logout">
-          <LogOutComponent {...this.props}/>
+          <LogOutComponent {...this.props} />
         </div>
 
         <div className="userList">
           <h1 className="h1">Users</h1>
 
-          <div>
+          <Nav tabs>
+            <NavItem>
+              <NavLink active={this.state.active_bool}>
+                <Button
+                  color="link"
+                  onClick={e => this.clickHandler(e)}
+                  value={true}>
+                  Active
+                </Button>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active={this.state.inactive_bool}>
+                <Button
+                  color="link"
+                  onClick={e => this.clickHandler(e)}
+                  value={false}>
+                  Inactive
+                </Button>
+              </NavLink>
+            </NavItem>
+          </Nav>
+          {/* <div>
             <ButtonGroup>
               <Button
+                //color="link"
                 onClick={e => this.clickHandler(e)}
                 value={true}
                 active={this.state.active_bool}>
                 Active
               </Button>
               <Button
+               // color="link"
                 onClick={e => this.clickHandler(e)}
                 value={false}
                 active={this.state.inactive_bool}>
                 Inactive
               </Button>
             </ButtonGroup>
-          </div>
+          </div> */}
 
           <div>
             <ReactTable
               data={this.state.data}
               pages={this.state.total_pages}
-              page={this.state.current_page-1}
+              page={this.state.current_page - 1}
               columns={columns}
               filterable
               noDataText="Please Wait ...."
               defaultPageSize={10}
               className="-striped -highlight"
-              onPageChange={(pageIndex) => this.changeCurrentPage(pageIndex)}
+              onPageChange={pageIndex => this.changeCurrentPage(pageIndex)}
               manual
               onFetchData={this.fetchData}
             />
--- residence-one/src/components/Sidebar/Sidebar.js
+++ residence-one/src/components/Sidebar/Sidebar.js
@@ -15,11 +15,30 @@ import {
 } from "reactstrap";
 import { Dashboard } from "../Dashboard/Dashboard";
 
+import {
+  FaRegCircle,
+  FaRegBuilding,
+  FaUser,
+  FaAtom,
+  FaUsers,
+  FaUserSecret,
+  FaCar,
+  FaFileAlt,
+  FaCalendarAlt,
+  FaRegNewspaper,
+  FaSwimmer,
+  FaRegCheckCircle,
+  FaRegAddressCard,
+  FaCogs,
+  FaGift,
+  FaFlagCheckered
+} from "react-icons/fa";
+
 export class Sidebar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      collapsed: true
+      collapsed:false
     };
     this.toggleNavbar = this.toggleNavbar.bind(this);
   }
@@ -33,7 +52,8 @@ export class Sidebar extends Component {
   render() {
     return (
       <div>
-        <Navbar
+           
+        {/* <Navbar
           color="faded"
           light
           style={{
@@ -60,7 +80,7 @@ export class Sidebar extends Component {
                 </Badge>
               </NavItem>
               <br />
-              <NavItem>
+             
                 <UncontrolledDropdown>
                   <DropdownToggle caret>Administration</DropdownToggle>
                   <DropdownMenu right>
@@ -75,39 +95,198 @@ export class Sidebar extends Component {
                     </DropdownItem>
                   </DropdownMenu>
                 </UncontrolledDropdown>
-              </NavItem>
+              
               <br />
-              <NavItem>
+              
                 <UncontrolledDropdown>
                   <DropdownToggle caret>Application</DropdownToggle>
                   <DropdownMenu right />
                 </UncontrolledDropdown>
-              </NavItem>
+            
               <br />
-              <NavItem>
+              
                 <UncontrolledDropdown>
                   <DropdownToggle caret>Council</DropdownToggle>
                   <DropdownMenu right />
                 </UncontrolledDropdown>
-              </NavItem>
+              
               <br />
 
-              <NavItem>
+              
                 <UncontrolledDropdown>
                   <DropdownToggle caret>Public</DropdownToggle>
-                  <DropdownMenu right>
                     <DropdownItem>
                       <Link to="/public/blogs">Blogs</Link>
                     </DropdownItem>
                     <DropdownItem>
                       <Link to="/public/website">Website</Link>
                     </DropdownItem>
-                  </DropdownMenu>
                 </UncontrolledDropdown>
-              </NavItem>
+              
             </Nav>
           </Collapse>
         </Navbar>
+         */}
+        
+        
+        <Navbar
+          color="faded"
+          light
+          style={{
+            position: "absolute",
+            backgroundColor: "#4c596e",
+            color: "red"
+          }}>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <div id="left" className="main-menu">
+            <div className="head">LA CADENELLE</div>
+            <Collapse isOpen={!this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret className="col">
+                    Administration
+                </DropdownToggle>
+                  <DropdownMenu right id="drop">
+                    <DropdownItem>
+                      <FaRegCircle id="link1" />
+                      <Link to="/administration/users" className="link">
+                        Users
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaRegBuilding id="link1" />{" "}
+                      <Link to="/administration/structure" className="link">
+                        Structure
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaUser id="link1" />{" "}
+                      <Link to="/administration/lots" className="link">
+                        Lots
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaAtom id="link1" />{" "}
+                      <Link to="/" className="link">
+                        Groups
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaUsers id="link1" />{" "}
+                      <Link to="/" className="link">
+                        Families
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaUserSecret id="link1" />{" "}
+                      <Link to="/" className="link">
+                        Owners
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaCar id="link1" />{" "}
+                      <Link to="/" className="link">
+                        Vehicle
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaFileAlt id="link1" />{" "}
+                      <Link to="/" className="link">
+                        Document
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaCalendarAlt id="link1" />{" "}
+                      <Link to="/" className="link">
+                        Event
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaRegNewspaper id="link1" />{" "}
+                      <Link to="/" className="link">
+                        News feed
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaSwimmer id="link1" />{" "}
+                      <Link to="/" className="link">
+                        Pool
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaRegCheckCircle id="link1" />{" "}
+                      <Link to="/" className="link">
+                        Issue Tracking
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaRegAddressCard id="link1" />{" "}
+                      <Link to="/" className="link">
+                        Contact
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaCogs id="link1" />
+                      <Link to="/" className="link">
+                        Settings
+                    </Link>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+            <Collapse isOpen={!this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret isOpen={this.state.isOpen} id="link1">
+                    Application
+                </DropdownToggle>
+                  <DropdownMenu right id="drop">
+                    <DropdownItem>
+                      <FaSwimmer id="link1" />{" "}
+                      <Link to="/apps/pools" className="link">
+                        Pool
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaGift id="link1" />{" "}
+                      <Link to="/apps/reception" className="link">
+                        Reception
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaFlagCheckered id="link1" />
+                      <Link to="/apps/reception" className="link">
+                        Notification
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaCar id="link1" />{" "}
+                      <Link to="/admin/pools" className="link">
+                        Vehicle
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaRegCheckCircle id="link1" />{" "}
+                      <Link to="/admin/pools" className="link">
+                        Issue Tracking
+                    </Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <FaRegAddressCard id="link1" />{" "}
+                      <Link to="/admin/pools" className="link">
+                        Contact
+                    </Link>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </div>
+
+          </Navbar>
+          
+        
       </div>
     );
   }

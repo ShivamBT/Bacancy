d117c15a234d0d22d054c93d8bcab803d773b944
ShivamBT <shivam.pande@bacancytechnology.com>
2019-03-15 19:42:28 +0530

    user details added

--- residence-one/src/components/Administration/UserDetails/DataHandler.js
+++ residence-one/src/components/Administration/UserDetails/DataHandler.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./UserDetails.css"
+
+
 export class DataHandler extends Component {
   constructor(props) {
     super(props);
     this.state = {
       active: ""
     };
+      
+      this.displayHandler = this.displayHandler.bind(this);
   }
 
   componentDidMount() {
@@ -20,13 +24,26 @@ export class DataHandler extends Component {
       this.setState({ active: this.props.active });
     }
   }
+    
+    displayHandler()
+    {
+        if (this.state.active === "profile")
+        {
+            return (<div>
+                <p>Profile pic</p>
+                <img src="" alt="Profile Picture" />
+            </div>)
+            
+            }
+         
+    }
 
   render() {
     return (
       <div style={{ backgroundColor: "red", color: "black" }} className="displayBlock">
         {console.log("return reached")}
         {this.state.active} is rendered;
-      
+        {this.displayHandler}
       </div>
     );
   }
